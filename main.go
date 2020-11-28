f3c0f3697807ad5d35ffad8d3b48fed77133ca0a
César <c.caycho46@gmail.com>
2020-11-28 18:57:28 -0500

    practica completa

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+type animal struct {
+	name  string
+	color string
+	bioma string
+}
+
 func main() {
 
 	archivo, err := os.OpenFile("databases.csv", os.O_RDWR, 0644)
@@ -25,5 +31,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(data)
+	animals := []animal{}
+
+	for i1 := range data {
+
+		animal := animal{name: data[i1][0], color: data[i1][1], bioma: data[i1][2]}
+
+		animals = append(animals, animal)
+	}
+
+	fmt.Println(animals)
 }
